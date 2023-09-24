78c70f71d7a30405edf2b6ad12bfdcc879c0d3c3
Thirza-12 <145908456+Thirza-12@users.noreply.github.com>
2023-09-24 18:23:29 +0530

    Update Day1.cpp

--- Day1.cpp
+++ Day1.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
 
+    // Subtract the product and sum of digits of an integer
+      
+    int lastdigit=0;
+    int multiplication=1;
+    int sum=0;
+    while(n!=0)
+    {
+        lastdigit=n%10;
+        multiplication=multiplication*lastdigit;
+        sum=sum+lastdigit;
+        n=n/10;
+    }
+    int ans=multiplication-sum;
+    return ans;
+    }
+};
