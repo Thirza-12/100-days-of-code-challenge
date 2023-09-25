533b8b440b55e6b8dde95ce9dd81c62a2cc894fb
Thirza-12 <145908456+Thirza-12@users.noreply.github.com>
2023-09-25 22:02:45 +0530

    Update Day2.cpp

--- Day2.cpp
+++ Day2.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    int reverse(int x) {
+      
+        // reverse integer
+      
+        long int rev=0;
+         int lastdigit=0;
+        while(x!=0)
+        {
+            lastdigit=x%10;
+            rev=(rev*10)+lastdigit;
+            if(rev>INT_MAX)return 0;
+            if(rev<INT_MIN)return 0;
+            x=x/10;
+        }
+        return(int) rev;
+    }
+};
