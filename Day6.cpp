1ab6deef06104803c134ffe6d822a1ead425b85d
Thirza Behal <145908456+Thirza-12@users.noreply.github.com>
2023-09-29 20:26:55 +0530

    Update Day6.cpp

--- Day6.cpp
+++ Day6.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+    // Power of two
+    int i=0;
+    while(i<=30)
+    {
+        int ans=pow(2,i);
+        if(ans==n)
+        {
+            return true;
+        }
+        i++;
+    }
+    return false;
+    } 
+};
