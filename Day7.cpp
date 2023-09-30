2995294fbacc46555c4adcfbe000940b5a43c54b
Thirza Behal <145908456+Thirza-12@users.noreply.github.com>
2023-09-30 21:20:37 +0530

    Update Day7.cpp

--- Day7.cpp
+++ Day7.cpp
@@ -1,1 +1,51 @@
+#include<iostream>
+using namespace std;
 
+// Program to find maximum and minimum elements in an array
+
+int getmin(int num[],int n)
+{
+    int mini=INT_MAX;
+    for(int i=0;i<n;i++)
+    {
+        mini=min(mini,num[i]);
+        // if(num[i]<min)
+        // {
+        //     min=num[i];
+        // }
+    }
+    // returning max value
+    return mini;
+}
+
+int getmax(int num[],int n)
+{
+    int maxi=INT_MIN;
+    for(int i=0;i<n;i++)
+    {
+        maxi=max(maxi,num[i]);
+        // if(num[i]>max)
+        // {
+        //     max=num[i];
+        // }
+    }
+    // returning max value
+    return maxi;
+}
+
+int main()
+{
+    int size;
+    cin>>size;
+    int num[2200];
+
+    // Taking input in array
+
+    for(int i=0;i<size;i++)
+    {
+        cin>>num[i];
+    }
+
+    cout<<"Maximum value is "<<getmax(num,size)<<endl;
+    cout<<"Mininum value is "<<getmin(num,size);
+}
