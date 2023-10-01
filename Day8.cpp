c7835c146867cbe2bd933223ce61262833258c7e
Thirza Behal <145908456+Thirza-12@users.noreply.github.com>
2023-10-01 21:51:24 +0530

    Update Day8.cpp

--- Day8.cpp
+++ Day8.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+using namespace std;
+int sumofarr(int arr[],int n)
+{
+    // sum of all elements in an array
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+        sum=sum+arr[i];
+    }
+    return sum;
+}
+int main()
+{
+    int size;
+    cin>>size;
+    int arr[100000];
+    for(int i=0;i<size;i++)
+    {
+        cin>>arr[i];
+    }
 
+    cout<<"sum of array elements are "<<sumofarr(arr,size);
+}
+  
