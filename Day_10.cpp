ae4c967be9922de0d20b3e29430d56cf5e0faedd
Thirza Behal <145908456+Thirza-12@users.noreply.github.com>
2023-10-03 23:07:28 +0530

    Update Day_10.cpp

--- Day_10.cpp
+++ Day_10.cpp
@@ -1,1 +1,41 @@
+#include<iostream>
+using namespace std;
 
+void printarray(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void reverse(int arr[],int n)
+{
+    int start=0;
+    int end=n-1;
+
+    while(start<=end)
+    {
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+}
+
+int main()
+{
+    //Reversing an array
+
+    int arr[100];
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+
+    reverse(arr,n);
+    
+    printarray(arr,n);
+}
