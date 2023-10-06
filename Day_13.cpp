10c6a1039767dcb96fe3577a0fc65a3d852c3a31
Thirza Behal <145908456+Thirza-12@users.noreply.github.com>
2023-10-06 21:09:03 +0530

    Update Day_13.cpp

--- Day_13.cpp
+++ Day_13.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        // Find the duplicate number
+        int ans;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n-1;i++)
+        {
+            if(nums[i]==nums[i+1])
+            {
+                ans=nums[i];
+                break;
+            }
+        }
+        return ans;
+    }
+};
