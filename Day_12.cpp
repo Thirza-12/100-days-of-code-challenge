dedf6972677e15941de3bcd466e97636d7ddf9a6
Thirza Behal <145908456+Thirza-12@users.noreply.github.com>
2023-10-06 21:05:45 +0530

    Update Day_12.cpp

--- Day_12.cpp
+++ Day_12.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main() {
+	// Complete the program of conacatenating two strings
+    string a;
+    cin>>a;
+    string b;
+    cin>>b;
+    int len=a.size();
+    int leng=b.size();
+    cout<<len<<" "<<leng<<endl;
+    cout<<a+b<<endl;
+    string c;
+    c=a;
+    c[0]=b[0];
+    cout<<c<<" ";
+    b[0]=a[0];
+    cout<<b;
+    
+    return 0;
+}
