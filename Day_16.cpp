4e5aaa383f2466426c51a6c4d2b885b94820cb1f
Thirza Behal <145908456+Thirza-12@users.noreply.github.com>
2023-10-16 10:15:53 +0530

    Update Day_16.cpp

--- Day_16.cpp
+++ Day_16.cpp
@@ -1,1 +1,18 @@
+int isSorted(int n, vector<int> a) {
+    // Write your code here.
+    vector<int>b=a;
+    sort(b.begin(),b.end());
+    for(int i=0;i<b.size();i++)
+    {
+        if(a[i]==b[i])
+        {
+            continue;
+        }
+        else
+        {
+            return 0;
+        }
+    }
 
+    return 1;
+}
