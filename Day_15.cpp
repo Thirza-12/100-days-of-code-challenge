507c0f95747ac898a268e4b94d2a425e441f0a20
Thirza Behal <145908456+Thirza-12@users.noreply.github.com>
2023-10-16 10:12:12 +0530

    Update Day_15.cpp

--- Day_15.cpp
+++ Day_15.cpp
@@ -1,1 +1,9 @@
-
+vector<int> getSecondOrderElements(int n, vector<int> a) {
+    // Write your code here.
+    int ans;
+    int ans2;
+    sort(a.begin(),a.end());
+    ans=a[n-2];
+    ans2=a[1];
+    return {ans,ans2};
+}
