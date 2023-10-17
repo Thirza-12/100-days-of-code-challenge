c44832d406a56aa3136f050bca2d08a5432bc4d4
Thirza Behal <145908456+Thirza-12@users.noreply.github.com>
2023-10-17 10:45:01 +0530

    Update Day_19.cpp

--- Day_19.cpp
+++ Day_19.cpp
@@ -1,1 +1,23 @@
+#include <bits/stdc++.h> 
+int pairSum(vector<int> &arr, int n, int target){
+	// Write your code here.
+	int count=0;
+	sort(arr.begin(),arr.end());
+	for(int i=0;i<arr.size();i++)
+	{
+		for(int j=i+1;j<arr.size();j++)
+		{
+			if(arr[i]+arr[j]==target)
+			{
+				count++;
+			}
+		}
+	}
 
+	if(count==0)
+	{
+		return -1;
+	}
+
+	return count;
+}
