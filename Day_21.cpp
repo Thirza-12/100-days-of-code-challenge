4f84dd2eed4d1c12465e842e7900d7023165d613
Thirza Behal <145908456+Thirza-12@users.noreply.github.com>
2023-10-19 14:22:34 +0530

    Update Day_21.cpp

--- Day_21.cpp
+++ Day_21.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+       int start=0;
+       int end= arr.size()-1;
+       int mid=(start+end)/2;
+       while(start<end)
+       {
+           if(arr[mid]<arr[mid+1])
+           {
+               start=mid+1;
+           }
+           else{
+               end=mid;
+           }
+           mid=(start+end)/2;
+       }
+       return start;
+    }
+};
