e9e568eac8053a9a8d1b04be94c66dabd984facd
Thirza Behal <145908456+Thirza-12@users.noreply.github.com>
2023-10-20 14:29:16 +0530

    Update Day_23.cpp

--- Day_23.cpp
+++ Day_23.cpp
@@ -1,1 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //calculator using switch case
 
+    int a;
+    cout<<"Enter the value of a ";
+    cin>>a;
+    int b;
+    cout<<"Enter the value of b ";
+    cin>>b;   
+    char op;
+    cout<<"Enter operation you want to perform ";
+    cin>>op;
+    switch (op)
+    {
+    case '+':cout<<"Addition: "<<a+b;
+        break;
+    case '-':cout<<"Subtraction: "<<a-b;
+        break;
+    case '*':cout<<"Multiplication: "<<a*b;
+        break;
+    case '/':cout<<"quotient:"<<a/b;
+        break;
+    case '%':cout<<"remainder: "<<a%b;
+        break;
+    default:cout<<"Enter valid operation to perform";
+        break;
+    }
+}
