5b88a768050024c080707db1024f735ce05a68e9
Thirza Behal <145908456+Thirza-12@users.noreply.github.com>
2023-10-20 11:25:43 +0530

    Update Day_22.cpp

--- Day_22.cpp
+++ Day_22.cpp
@@ -1,1 +1,11 @@
-
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+       // int ans=0;
+       if(n==0) return 1;
+        for(int i=1;i<=n;i=i*2){
+            n=n^i;
+        }
+        return n;
+    }
+};
