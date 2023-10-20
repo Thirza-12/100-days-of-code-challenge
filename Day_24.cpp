2cc497d0520caff2396504b7ecf8ceba98fc8584
Thirza Behal <145908456+Thirza-12@users.noreply.github.com>
2023-10-20 11:27:27 +0530

    Update Day_24.cpp

--- Day_24.cpp
+++ Day_24.cpp
@@ -1,1 +1,26 @@
-
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n=nums.size();
+        int start=0;
+        int end=n-1;
+        int mid=(start+end)/2;
+        if(target>nums[n-1])
+        {
+            return n;
+        }
+        while(start<end)
+        {
+            if(nums[mid]<target)
+            {
+                start=mid+1;
+            }
+            else if(nums[mid]>=target)
+            {
+                end=mid;
+            }
+            mid=(start+end)/2;
+        }
+        return start;
+    }
+};
