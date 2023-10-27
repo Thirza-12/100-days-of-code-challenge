dc90c6a1d45d83f0723b5b7f56366b5909eab088
Thirza Behal <145908456+Thirza-12@users.noreply.github.com>
2023-10-27 11:22:41 +0530

    Create Day_31.cpp

---
+++ Day_31.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int sumOfSquares(vector<int>& nums) {
+        int ans=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if(n%(i+1)==0)
+            {
+                int square=nums[i]*nums[i];
+                ans=ans+square;
+            }
+        }
+
+        return ans;
+    }
+};
