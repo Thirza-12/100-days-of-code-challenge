2f7b5cf349a1b3ccc83e67ceadafb62be9fee426
Thirza Behal <145908456+Thirza-12@users.noreply.github.com>
2023-11-05 13:31:07 +0530

    Update Day_39.cpp

--- Day_39.cpp
+++ Day_39.cpp
@@ -1,1 +1,18 @@
+class Solution {
+public:
+    int countDigits(int num) {
+        int count=0;
+        int n=num;
+        while(num!=0)
+        {
+            int rem=num%10;
+            if(n%rem==0)
+            {
+                count++;
+            }
+            num=num/10;
+        }
 
+        return count;
+    }
+};
