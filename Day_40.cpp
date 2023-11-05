55ac49742d171bfa862d72e0fec56053acb354c0
Thirza Behal <145908456+Thirza-12@users.noreply.github.com>
2023-11-05 13:32:41 +0530

    Update Day_40.cpp

--- Day_40.cpp
+++ Day_40.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int ans=0;
+        for(int i=1;i<=num/2;i++)
+        {
+            if(num%i==0)
+            {
+                ans=ans+i;
+            }
+        }
 
+        if(ans==num)
+        {
+            return true;
+        }
+        else
+        {
+        return false;
+        }
+
+    }
+};
