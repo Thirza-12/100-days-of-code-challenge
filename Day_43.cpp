6a1a34e0a5567fda2a7905582501a2d5726205a0
Thirza Behal <145908456+Thirza-12@users.noreply.github.com>
2023-11-07 10:19:02 +0530

    Update Day_43.cpp

--- Day_43.cpp
+++ Day_43.cpp
@@ -1,1 +1,12 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for(int i=0;i<s.size();i++)
+        {
+            s[i]=tolower(s[i]);
+        }
 
+        return s;
+        
+    }
+};
