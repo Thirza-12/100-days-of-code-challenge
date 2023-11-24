f755d5cfff4889515b55d60b6865440c4def332c
Thirza Behal <145908456+Thirza-12@users.noreply.github.com>
2023-11-24 09:28:11 +0530

    Create Day_61.cpp

---
+++ Day_61.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+int findDuplicate(vector<int> &arr, int n){
+	// Write your code here.
+	sort(arr.begin(),arr.end());
+	for(int i=0;i<n;i++)
+	{
+		for(int j=1;j<n;j++)
+		{
+			if(arr[i]==arr[i+1])
+			{
+				return arr[i];
+			}
+		}
+	}
+}
