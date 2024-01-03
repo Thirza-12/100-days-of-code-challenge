0edf6f3dc721b5b7001d6bddcd1e33b3aaf90433
Thirza Behal <145908456+Thirza-12@users.noreply.github.com>
2024-01-03 21:05:36 +0530

    Create Day_94.cpp

---
+++ Day_94.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countKDifference(vector<int>& nums, int k) {
+        int count=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=1;j<nums.size();j++)
+            {
+                if(i<j && abs(nums[i]-nums[j])==k)
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
